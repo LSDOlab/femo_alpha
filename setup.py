60af9156b7af0af7cb6233f7e282817f424be0b0
Rurux <xiangrurose@gmail.com>
2024-06-24 15:39:16 -0700

    Added the lsdo template.

--- setup.py
+++ setup.py
@@ -1,20 +1,56 @@
-import os
-import codecs
 from setuptools import setup, find_packages
 
-base_dir = os.path.abspath(os.path.dirname(__file__))
-def read(fname):
-    return codecs.open(os.path.join(base_dir, fname), encoding="utf-8").read()
+import codecs
+import os.path
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='femo_alpha',
-    version='0.1',
-    packages=find_packages(),
-    url='https://github.com/LSDOlab/femo_alpha',
-    license='GNU LGPLv3',
+    version=get_version('femo_alpha/__init__.py'),
     author='Ru Xiang',
     author_email='rxiang@ucsd.edu',
-    description="Finite Element for Multidisciplinary Optimization",
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
+    license='LGPLv3+',
+    url='http://github.com/LSDOlab/femo_alpha',
+    download_url='http://pypi.python.org/pypi/femo_alpha',
+    description='Finite element methods for multidisciplinary optimization.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    python_requires='>=3.9',
+    platforms=['any'],
+    install_requires=[
+        'numpy',
+        'pytest',
+        'scipy',
+        'h5py',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries',
+    ],
 )
